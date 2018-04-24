d836d93154cd8c36ad0d1a7582d29e18cf211271
SteffeyDev <steffeydev@icloud.com>
2018-04-24 10:53:56 -0400

    fixing sign script again

--- .travis/sign.sh
+++ .travis/sign.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  export CERTIFICATE_P12=Certificate.p12;
+  CERTIFICATE_P12=Certificate.p12;
   echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12;
-  export KEYCHAIN=build.keychain;
+  KEYCHAIN=build.keychain;
   security create-keychain -p mysecretpassword $KEYCHAIN;
   security default-keychain -s $KEYCHAIN;
   security unlock-keychain -p mysecretpassword $KEYCHAIN;
-  echo $CERTIFICATE_P12
-  echo $KEYCHAIN
-  echo $(which codesign)
+  echo $CERTIFICATE_P12;
+  echo $KEYCHAIN;
+  echo $(which codesign);
+  cat $CERTIFICATE_P12;
   security import $CERTIFICATE_P12 -k $KEYCHAIN -P "" -T $(which codesign);
 
   codesign --force --sign "Developer ID Application" atemOSC.app/Contents/Frameworks/VVBasics.framework/Versions/Current
